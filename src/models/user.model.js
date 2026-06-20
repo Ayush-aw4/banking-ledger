3ef3cb2c5ff9ac0e50acbcce7326e3fa6deb0718
AYUSH AWCHAR <ayushawchar9403@gmail.com>
2026-06-20 21:43:30 +0530

    added user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: [true,"Email is required"],
+        trim: true,
+        lowercase: true,
+        match : [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Invalid email address"],
+        unique : [true,"Email already exist."]
+    },
+    name:{
+        type: String,
+        required: [true,"Nmae is required"],
+    },
+    password:{
+        type:String,
+        required: [true,"Password is required"],
+        minlength: [6,"password should contian more than 6 charcters"],
+        select: false
+    }
+},
+{timestamps:true})
+
+userSchema.pre("save",async function(next){
+ if(!this.isModified("password")){
+    return next();
+ }
+ const hash = await bcrypt.hash(this.password,10)
+ this.password = hash
+ return next()
+})
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+const userModel = mongoose.model("user",userSchema)
+module.exports = userModel
